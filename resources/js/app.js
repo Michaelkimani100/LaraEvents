00d05ce89ee4e95b1079d3a696fc07d4ed613d00
Michaelkimani100 <michaelkimani854@gmail.com>
2019-09-28 17:59:25 +0300

    Front-end setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,20 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { Form, HasError, AlertError } from 'vform'
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
+window.Form = Form
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('product', require('./components/Product.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +22,25 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+let routes = [
+
+    { path: '/product', component: require('./components/Product.vue').default },
+
+
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+
+
+
+
+
+
 const app = new Vue({
     el: '#app',
+    router
 });
---
+++ resources/js/components/Product.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center text-gold"><h3>Add Product</h3></div>
+
+                    <div class="card-body">
+                       <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Name</label>
+      <input type="text" class="form-control" placeholder="Product name" name="name" v-model="form.name">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Quantity</label>
+      <input type="number" class="form-control"  placeholder="Available quantity" name="quantity" v-model="form.quantity">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Reorder Level</label>
+    <input type="number" class="form-control"  placeholder="Reorder level" name="reorder" v-model="form.reorder">
+  </div>
+  <button type="submit" class="btn btn-primary float-right" @click.prevent="addProduct">Add</button>
+</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                form:new Form({
+                    name:'',
+                    quantity:'',
+                    reorder:''
+
+
+                })
+            }
+
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods:{
+            addProducts()
+            {
+                this.form.post('api/product')
+                .then(()=>{
+                    console.log('Added')
+
+                })
+                .catch(()=>{
+                    console.log('error')
+                })
+
+            }
+        }
+    }
+</script>
+<style scoped>
+.card{
+    background:whitesmoke;
+}
+.btn{
+background: #DAA520;
+border: none;
+}
+</style>
